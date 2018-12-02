a123b81b102329d694979dc5c8f7b48706f95477
Adonais0 <chuyao@umich.edu>
2018-12-01 21:29:52 -0500

    add frontend

--- Search_Website.py
+++ Search_Website.py
@@ -4,45 +4,47 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    hero_pic=""
-    myhero="nochosen"
-    xx=""
-    return render_template("hero.html", heros=HEROS,hero_pic=hero_pic,myhero=myhero,results=xx)
-
-# @app.route('/hero',methods=['GET','POST'])
-# def hero():
-#     if request.method=='POST':
-#         myhero=request.form['hero']
-#         results=process_top_10_decks(myhero)
-#         xx=[]
-#         yy=[]
-#         for each in results:
-#             deckname=str(each.get_deckname())
-#             deckscore=int(each.get_deckscore())
-#             xx.append(deckname)
-#             yy.append(deckscore)
-#         Data = [go.Bar(x=xx,y=yy)]
-#         fig = go.Figure(data=Data)
-#         hero_pic = plotly.offline.plot(fig, show_link=False, output_type="div", include_plotlyjs="static/plotly-latest.min.js")
-#     else:
-#         hero_pic=""
-#         myhero="nochosen"
-#         xx=""
-#     return render_template("hero.html", heros=HEROS,hero_pic=hero_pic,myhero=myhero,results=xx)
-
-# @app.route('/deck',methods=['GET','POST'])
-# def deck():
-#     deckname='Jade Druid Deck List Guide (Post Nerf) – Kobolds – April 2018'
-#     chartname=""
-#     if request.method=='POST':
-#         deckname=request.form['deckname']
-#         chartname=request.form['chartname']
-#         mychart=getchart(deckname,chartname)
-#     else:
-#         mychart=""
-#     return render_template("deckanalysize.html", deckname=deckname,chartname=chartname,mychart=mychart)
+    if request.method == 'POST':
+        searchContent = request.form['searchContent']
+        print(searchContent)
+    return render_template("index.html")
 
 
+@app.route('/result', methods =['GET','POST'])
+def result():
+    if request.method == 'POST':
+        searchContent = request.form['searchContent']
+        print(searchContent)
+
+    problems = [
+        {
+            'title': '3sum problem',
+            'tags': ['tag1', 'tag2'],
+            'difficulty': 'hard',
+            'discussions': [
+                {'discussion_title': 'test discussion', 'url': 'https://leetcode.com/problems/integer-to-english-words/discuss/70625/My-clean-Java-solution-very-easy-to-understand'},
+                {'discussion_title': 'test discussion', 'url': 'https://leetcode.com/problems/integer-to-english-words/discuss/70625/My-clean-Java-solution-very-easy-to-understand'},
+                {'discussion_title': 'test discussion', 'url': 'https://leetcode.com/problems/integer-to-english-words/discuss/70625/My-clean-Java-solution-very-easy-to-understand'},
+            ],
+        },
+        {
+            'title': '3sum problem',
+            'tags': ['tag1', 'tag2'],
+            'difficulty': 'hard',
+            'discussions': [
+                {'discussion_title': 'test discussion', 'url': 'https://leetcode.com/problems/integer-to-english-words/discuss/70625/My-clean-Java-solution-very-easy-to-understand'},
+            ],
+        },
+        {
+            'title': '3sum problem',
+            'tags': ['tag1', 'tag2'],
+            'difficulty': 'hard',
+            'discussions': [
+                {'discussion_title': 'test discussion', 'url': 'https://leetcode.com/problems/integer-to-english-words/discuss/70625/My-clean-Java-solution-very-easy-to-understand'},
+            ],
+        },
+    ]
+    return render_template("result.html", problems = problems)
 
 if __name__ == '__main__':
 
